c1526cff825a164483335b1d317d3b4d07ed2d20
lawder <xmvagrant@gmail.com>
2018-01-16 10:09:17 +0800

    update headers

--- HappyDNS-dynamic/HappyDNS.h
+++ HappyDNS-dynamic/HappyDNS.h
@@ -6,35 +6,14 @@
 //  Copyright © 2018年 Qiniu Cloud Storage. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <QNRefresher.h>
-#import <QNAssessment.h>
-#import <QNRefresher.h>
-#import <QNHex.h>
-#import <QNDes.h>
-#import <QNIP.h>
-#import <QNGetAddrInfo.h>
-#import <QNDnsManager.h>
-#import <QNRecord.h>
-#import <QNDomain.h>
-#import <QNNetworkInfo.h>
-#import <QNLruCache.h>
-#import <QNResolvUtil.h>
-#import <QNResolver.h>
-#import <QNHosts.h>
-#import <QNHijackingDetectWrapper.h>
-#import <QNTxtResolver.h>
-#import <QNDnspodEnterprise.h>
-#import <QNDnspodFree.h>
-
-
-
-//! Project version number for HappyDNS.
-FOUNDATION_EXPORT double HappyDNSVersionNumber;
-
-//! Project version string for HappyDNS.
-FOUNDATION_EXPORT const unsigned char HappyDNSVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <HappyDNS/PublicHeader.h>
-
-
+#import <HappyDNS/QNIP.h>
+#import <HappyDNS/QNDomain.h>
+#import <HappyDNS/QNRecord.h>
+#import <HappyDNS/QNResolver.h>
+#import <HappyDNS/QNDnspodFree.h>
+#import <HappyDNS/QNDnsManager.h>
+#import <HappyDNS/QNGetAddrInfo.h>
+#import <HappyDNS/QNNetworkInfo.h>
+#import <HappyDNS/QNResolverDelegate.h>
+#import <HappyDNS/QNDnspodEnterprise.h>
+#import <HappyDNS/QNHijackingDetectWrapper.h>
