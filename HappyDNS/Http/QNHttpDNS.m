489ed666016fe6e845ce87a702a73765946adfb0
kivenhaoyu <1132628199@qq.com>
2018-03-08 17:30:58 +0800

    add http dns

---
+++ HappyDNS/Http/QNHttpDNS.m
@@ -0,0 +1,125 @@
+//
+//  QNHttpDNS.m
+//  HappyDNS
+//
+//  Created by 何昊宇 on 2018/3/6.
+//  Copyright © 2018年 Qiniu Cloud Storage. All rights reserved.
+//
+
+#import "QNHttpDNS.h"
+#import "QNDomain.h"
+#import "QNRecord.h"
+#import "QNNetworkInfo.h"
+#import "QNDes.h"
+#import "QNHex.h"
+
+#define ENDPOINT @"https://httpdns.qnydns.net/"
+
+@interface QNHttpDNS()
+
+@property (nonatomic, strong) QNDes *des;
+
+@end
+
+@implementation QNHttpDNS
+
+- (instancetype)initWithAccountId:(NSString *)accountId {
+    return [self initWithAccountId:accountId encryptKey:nil expireTime:0];
+}
+
+- (instancetype)initWithAccountId:(NSString *)accountId
+                       encryptKey:(NSString *)encryptKey
+                       expireTime:(long) expireTime {
+    if (self = [super init]) {
+        _accountId = accountId;
+        _expireTime = expireTime;
+        if(encryptKey) {
+            _encryptKey = encryptKey;
+            _des = [[QNDes alloc] init:[encryptKey dataUsingEncoding:NSUTF8StringEncoding]];
+        }
+    }
+    return self;
+}
+
+- (NSString *)encrypt:(NSString *)domain {
+    NSData *data = [_des encrypt:[domain dataUsingEncoding:NSUTF8StringEncoding]];
+    if (data == nil) {
+        return nil;
+    }
+    NSString *str = [QNHex encodeHexData:data];
+    return str;
+}
+
+- (NSArray *)decrypt:(NSData *)raw {
+    NSData *enc = [QNHex decodeHexString:[NSString stringWithUTF8String:[raw bytes]]];
+    if (enc == nil) {
+        return nil;
+    }
+    NSData *data = [_des decrpyt:enc];
+    if (data == nil) {
+        return nil;
+    }
+    return [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+}
+
+- (NSArray *)query:(QNDomain *)domain networkInfo:(QNNetworkInfo *)netInfo error:(NSError *__autoreleasing *)error {
+    
+    NSString * realDomain = domain.domain;
+    if (self.encryptKey) {
+        realDomain = [self encrypt:[NSString stringWithFormat:@"%@?e=%ld",domain.domain,(long)[[NSDate date] timeIntervalSince1970] + self.expireTime]];
+        if (realDomain == nil) {
+            if (error != nil) {
+                *error = [[NSError alloc] initWithDomain:domain.domain code:kQN_ENCRYPT_FAILED userInfo:nil];
+            }
+            return nil;
+        }
+    }
+    NSString *url = [NSString stringWithFormat:@"%@%@/d?dn=%@&ttl=1", ENDPOINT,self.accountId,realDomain];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:url] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:QN_DNS_DEFAULT_TIMEOUT];
+    NSHTTPURLResponse *response = nil;
+    NSError *httpError = nil;
+    NSData* data = [NSURLConnection sendSynchronousRequest:urlRequest
+                                         returningResponse:&response
+                                                     error:&httpError];
+    
+    if (httpError != nil) {
+        if (error != nil) {
+            *error = httpError;
+        }
+        return nil;
+    }
+    if (response.statusCode != 200) {
+        return nil;
+    }
+    
+    NSArray *raw;
+    if (self.encryptKey) {
+        NSData * stringData = [data subdataWithRange:NSMakeRange(0, data.length-1)];
+        raw = [self decrypt:stringData];
+    }else {
+        raw = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+    }
+    if (raw == nil) {
+        if (error != nil) {
+            *error = [[NSError alloc] initWithDomain:domain.domain code:kQN_DECRYPT_FAILED userInfo:nil];
+        }
+        return nil;
+    }
+    NSDictionary * rawDic = raw[0];
+    NSArray *result = [rawDic objectForKey:@"A"];
+    if (result.count <= 0) {
+        return nil;
+    }
+    int ttl = [[rawDic objectForKey:@"ttl"] integerValue];
+    if (ttl <= 0) {
+        return nil;
+    }
+    NSMutableArray *ret = [[NSMutableArray alloc] initWithCapacity:result.count];
+    for (int i = 0; i < result.count; i++) {
+        QNRecord *record = [[QNRecord alloc] init:[result objectAtIndex:i] ttl:ttl type:kQNTypeA];
+        [ret addObject:record];
+    }
+    return ret;
+}
+
+@end
---
+++ HappyDNSTests/httpDNSTest.m
@@ -0,0 +1,64 @@
+//
+//  httpDNSTest.m
+//  HappyDNS
+//
+//  Created by 何昊宇 on 2018/3/6.
+//  Copyright © 2018年 Qiniu Cloud Storage. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "QNDomain.h"
+#import "QNRecord.h"
+#import "QNResolverDelegate.h"
+#import "QNHttpDNS.h"
+
+@interface httpDNSTest : XCTestCase
+
+@end
+
+@implementation httpDNSTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testHttpDNSWithEncryptKey {
+    id<QNResolverDelegate> resolver = [[QNHttpDNS alloc] initWithAccountId:@"123456" encryptKey:@"9033480eb8b04b695aa7001ae1d01352" expireTime:3600];
+    NSArray *records = [resolver query:[[QNDomain alloc] init:@"www.baidu.com"] networkInfo:nil error:nil];
+    XCTAssert(records != nil, @"Pass");
+    XCTAssert(records.count > 0, @"Pass");
+    XCTAssert(records.count >= 1, @"Pass");
+    QNRecord *record = [records objectAtIndex:0];
+    XCTAssert(record.ttl >= 0, @"Pass");
+    
+    records = [resolver query:[[QNDomain alloc] init:@"www.qiniu.com"] networkInfo:nil error:nil];
+    XCTAssert(records != nil, @"Pass");
+    XCTAssert(records.count >= 1, @"Pass");
+    record = [records objectAtIndex:0];
+    XCTAssert(record.ttl >= 0, @"Pass");
+}
+
+- (void)testHttpDNS {
+    id<QNResolverDelegate> resolver = [[QNHttpDNS alloc] initWithAccountId:@"123456"];
+    NSArray *records = [resolver query:[[QNDomain alloc] init:@"www.baidu.com"] networkInfo:nil error:nil];
+    XCTAssert(records != nil, @"Pass");
+    XCTAssert(records.count > 0, @"Pass");
+    XCTAssert(records.count >= 1, @"Pass");
+    QNRecord *record = [records objectAtIndex:0];
+    XCTAssert(record.ttl >= 0, @"Pass");
+    
+    records = [resolver query:[[QNDomain alloc] init:@"www.qiniu.com"] networkInfo:nil error:nil];
+    XCTAssert(records != nil, @"Pass");
+    XCTAssert(records.count >= 1, @"Pass");
+    record = [records objectAtIndex:0];
+    XCTAssert(record.ttl >= 0, @"Pass");
+}
+
+
+@end
