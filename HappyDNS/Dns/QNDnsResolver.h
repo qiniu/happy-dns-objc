ef10972b230cc69e2d1ae84a2401a7a588d7d0bb
yangsen <yang_sen_ysys@163.com>
2021-07-28 15:55:41 +0800

    add api desc

--- HappyDNS/Dns/QNDnsResolver.h
+++ HappyDNS/Dns/QNDnsResolver.h
@@ -18,9 +18,19 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nonatomic, assign, readonly)int timeout;
 @property(nonatomic,   copy, readonly)NSArray *servers;
 
+/// 构造函数
+/// @param server 指定 dns server
+///                 1. UdpResolver时 server 为 ip。 eg:8.8.8.8
+///                 2. Doh 时 server 为 url。 eg:https://dns.google/dns-query
+/// @param timeout 超时时间
 + (instancetype)resolverWithServer:(NSString *)server
                            timeout:(int)timeout;
 
+/// 构造函数
+/// @param serverIPs 指定多个 dns server，同时进行 dns 解析，当第一个有效数据返回时结束，或均为解析到数据时结束
+///                 1. UdpResolver时 server 为 ip。 eg:8.8.8.8
+///                 2. Doh 时 server 为 url。 eg:https://dns.google/dns-query
+/// @param timeout 超时时间
 + (instancetype)resolverWithServers:(NSArray <NSString *> *)servers
                             timeout:(int)timeout;
 
--- HappyDNS/Dns/QNDnsUdpResolver.h
+++ HappyDNS/Dns/QNDnsUdpResolver.h
@@ -11,12 +11,22 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface QNDnsUdpResolver : QNDnsResolver
 
+/// 构造函数
+/// @param serverIP 指定 dns local server1. eg:8.8.8.8
+/// @param timeout 超时时间
 + (instancetype)resolverWithServerIP:(NSString *)serverIP
                              timeout:(int)timeout;
 
+/// 构造函数
+/// @param serverIPs 指定多个 dns local server，同时进行 dns 解析，当第一个有效数据返回时结束，或均为解析到数据时结束. eg:@[@"8.8.8.8"]
+/// @param timeout 超时时间
 + (instancetype)resolverWithServerIPs:(NSArray <NSString *> *)serverIPs
                               timeout:(int)timeout;
 
+/// 构造函数
+/// @param serverIPs 指定多个 dns local server，同时进行 dns 解析，当第一个有效数据返回时结束，或均为解析到数据时结束. eg:@[@"8.8.8.8"]
+/// @param queue 多个 udp 包所在的 queue
+/// @param timeout 超时时间
 + (instancetype)resolverWithServerIPs:(NSArray <NSString *> *)serverIPs
                                 queue:(dispatch_queue_t _Nullable)queue
                               timeout:(int)timeout;
